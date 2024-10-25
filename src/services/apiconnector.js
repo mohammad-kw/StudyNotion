c86e294407f03e4f97a372852b5b5f150ecfdc2e
MohammadSaifee <mohammed53saifee@gmail.com>
2024-10-25 19:17:52 +0530

    updated apiconnector

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -18,7 +18,7 @@
 import axios from "axios";
 
 // Set the base URL for API requests
-const baseURL = process.env.REACT_APP_API_BASE_URL || "http://localhost:4000"; // Fallback to localhost for local development
+const baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:4000"; // Fallback to localhost for local development
 
 export const axiosInstance = axios.create({
     baseURL, // Use the base URL here
