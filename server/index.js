03e18db720d6cbf2051ea06889e0788e75d29ef9
MohammadSaifee <mohammed53saifee@gmail.com>
2024-10-25 17:34:00 +0530

    handled cors

--- server/index.js
+++ server/index.js
@@ -26,7 +26,8 @@ app.use(express.urlencoded({extended: true}))//for using postman
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		// origin:"http://localhost:3000",
+		origin:"https://studynotion-frontend-six-henna.vercel.app",
 		credentials:true,
 	})
 )
